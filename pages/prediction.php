a743fe48d281e4e67d740c7442acc0361a64f8c5
flakyriemaid <valkkyriemaid@gmail.com>
2022-01-02 16:44:02 +0700

    fixing visual glitch, remove redundant feature

--- pages/prediction.php
+++ pages/prediction.php
@@ -62,8 +62,6 @@
                 <a href="#" class="drop_fill" id="humidity">Humidity</a>
                 <hr>
                 <a href="#" class="drop_fill" id="pressure">Pressure</a>
-                <hr>
-                <a href="#" class="drop_fill" id="recommend">Recommendation</a>
             </div>
         </div>
     </section>
